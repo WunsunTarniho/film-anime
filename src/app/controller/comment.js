7771b08f51e6fc1ebb30d490b5aad7d404f664d7
wunsun5 <wunsun58@gmail.com>
2024-05-25 10:08:30 +0700

    third commit

--- src/app/controller/comment.js
+++ src/app/controller/comment.js
@@ -1,11 +1,11 @@
 class Comment {
     static async all(req) {
-        const response = await fetch(`${process.env.NEXTAUTH_URL}/api/v1/comment?anime_id=${req.anime_id}`);
+        const response = await fetch(`/api/v1/comment?anime_id=${req.anime_id}`);
         return await response.json();
     }
 
     static async store(req) {
-        const response = await fetch(`${process.env.NEXTAUTH_URL}/api/v1/comment`, {
+        const response = await fetch(`/api/v1/comment`, {
             method: 'POST',
             body: JSON.stringify(req)
         });
